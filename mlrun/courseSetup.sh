9bb0187df52706b1dda03711ea43d71468c96710
Marcelo Litovsky <12512887+marcelonyc@users.noreply.github.com>
2020-04-09 14:47:14 -0400

    Added mkdir

--- mlrun/courseSetup.sh
+++ mlrun/courseSetup.sh
@@ -17,12 +17,21 @@ kubectl run nginx --image=nginx --replicas=1
 
 kubectl get pods >>  deployment.log 2>&1
 
+mkdir /tmp/mlrun
+mkdir /tmp/mlrun/data
+
 echo "Build image"
 docker build -t mlrun/jupy - < Dockerfile.jupy >>  deployment.log   2>&1
 
 echo "Deploy Jupyter"
 kubectl apply -f mljupy.yaml >>  deployment.log   2>&1
-i
+
+kubectl get pods|grep jupy|grep Runn > /dev/null
+while [ $? -ne 0 ]
+do
+     sleep 10
+     kubectl get pods|grep jupy|grep Runn > /dev/null
+done
 echo "+++++++++++++++++++++" >>  deployment.log   2>&1
 echo "DEPLOYMENT COMPLETED"  >>  deployment.log   2>&1
 echo "+++++++++++++++++++++" >>  deployment.log   2>&1
--- mlrun/step1.md
+++ mlrun/step1.md
@@ -1,7 +1,7 @@
 ## Wait for deployment to complete
 
-Run this command and wait until you "DEPLOYMENT COMPLETED"
+Run this command and wait until you see "DEPLOYMENT COMPLETED"
 
-Deployment takes about 5 minutes to complete
+Deployment takes about 15 minutes to complete. Grab a coffe and meet me back here in 15.
 
 `tail -f deployment.log`{{execute}}
