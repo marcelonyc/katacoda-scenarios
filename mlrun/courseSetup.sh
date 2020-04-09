4eeba02d252fee512d64e20cbdf1e3a866391304
Marcelo Litovsky <12512887+marcelonyc@users.noreply.github.com>
2020-04-09 11:36:45 -0400

    Added wait for Kubernetes

--- mlrun/courseSetup.sh
+++ mlrun/courseSetup.sh
@@ -1,2 +1,9 @@
 git clone http://github.com/mlrun/mlrun
+# Wait for Kubernetes to come up
+kubectl get pods > /dev/null
+while [ $? -ne 0 ]
+do
+        sleep 10
+	kubectl get pods > /dev/null
+done
 kubectl run nginx --image=nginx --replicas=1
