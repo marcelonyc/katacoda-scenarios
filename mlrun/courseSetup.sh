cfcccf5d03c78e8a0116c1c72fbef72f4f35f4be
Marcelo Litovsky <12512887+marcelonyc@users.noreply.github.com>
2020-04-09 11:58:17 -0400

    Log redirect

--- mlrun/courseSetup.sh
+++ mlrun/courseSetup.sh
@@ -1,5 +1,8 @@
-git clone http://github.com/mlrun/mlrun
 # Wait for Kubernetes to come up
+
+exec 2>&1
+exec >> deployment.log
+
 kubectl get pods > /dev/null
 while [ $? -ne 0 ]
 do
@@ -7,5 +10,6 @@ do
 	kubectl get pods > /dev/null
 done
 kubectl run nginx --image=nginx --replicas=1
+
 
 cd k8s/image && docker build -t mlrun/jupy . && kubectl apply -f mljupy.yaml
