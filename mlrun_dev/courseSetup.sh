9f64da7138007b57d4882e67bf0be475dcae40c7
Marcelo Litovsky <12512887+marcelonyc@users.noreply.github.com>
2020-04-12 08:03:02 -0400

    Consolidate api and jupyter containers

--- mlrun_dev/courseSetup.sh
+++ mlrun_dev/courseSetup.sh
@@ -2,6 +2,7 @@
 
 # Wait for Kubernetes to come up
 
+
 exec 2>&1
 exec >> deployment.log
 
@@ -19,29 +20,21 @@ kubectl apply -f pv_local.yaml >> deployment.log   2>&1
 echo "Create Nginix"
 kubectl run nginx --image=nginx --replicas=1  deployment.log 2>&1
 
-kubectl get pods >>  deployment.log 2>&1
 
-mkdir -p /tmp/mlrun/mlrun
+mkdir -p /tmp/mlrun/mlrun_course
 mkdir /tmp/mlrun/data
+
+sleep 2
 
 ./setup_registry.sh >> deployment.log   2>&1
 
-echo "Build image"
-##docker build -t mlrun/jupy - < Dockerfile.jupy >>  deployment.log   2>&1
 
 echo "Deploy Jupyter"
-kubectl apply -f mljupy.yaml >>  deployment.log   2>&1
+./setup_jupyter.sh   >> deployment.log   2>&1
 
 
 echo "Deploy MLRun API"
-./setup_mlrunapi.sh >> deployment.log   2>&1
-
-kubectl get pods|grep mlrun-ui > /dev/null
-while [ $? -ne 0 ]
-do
-     sleep 10
-     kubectl get pods|grep mlrun-ui > /dev/null
-done
+./setup_mlrunui.sh >> deployment.log   2>&1
 
 kubectl get pods|grep jupy|grep Runn > /dev/null
 while [ $? -ne 0 ]
@@ -49,7 +42,7 @@ do
      sleep 10
      kubectl get pods|grep jupy|grep Runn > /dev/null
 done
-chown -R 1000 /tmp/mlrun
+chown -R 1000 /tmp/mlrun/mlrun_course
 clear
 echo
 echo
