7351511bd9db490ea58fcd75f0bfe836d1b04238
Marcelo Litovsky <12512887+marcelonyc@users.noreply.github.com>
2020-04-12 08:18:12 -0400

    added mlrunapi env to jupy yaml

--- mlrun_dev/assets/setup_jupyter.sh
+++ mlrun_dev/assets/setup_jupyter.sh
@@ -1,3 +1,4 @@
 #! /bin/bash
-
-kubectl apply -f mljupy.yaml >>  deployment.log   2>&1
+IP=`hostname -I|cut -f 1 -d " "`
+cat mljupy.yaml | sed s/{{REGISTRY}}/${IP}:80/g > /tmp/mlrunapi.yaml
+kubectl apply -f /tmp/mlrunapi.yaml >>  deployment.log   2>&1
