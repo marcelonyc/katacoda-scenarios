2049b21bba57d741f6617b6c74879be490df57a6
Marcelo Litovsky <12512887+marcelonyc@users.noreply.github.com>
2020-04-12 09:52:17 -0400

    Correct kubectl syntax error

--- mlrun_dev/courseSetup.sh
+++ mlrun_dev/courseSetup.sh
@@ -58,7 +58,7 @@ echo
 cp *.ipynb /tmp/mlrun/mlrun_course/.
 cp start_mlrun.sh /tmp/mlrun/mlrun_course/.
 
-kubectl exec -d `kubectl get pods |grep jupyter |awk '{print $1}'` /home/jovyan/mlrun/mlrun_course/start_mlrun.sh
+kubectl exec  `kubectl get pods |grep jupyter |awk '{print $1}'` /home/jovyan/mlrun/mlrun_course/start_mlrun.sh
 
 echo "+++++++++++++++++++++" >>  deployment.log   2>&1
 echo "DEPLOYMENT COMPLETED"  >>  deployment.log   2>&1
